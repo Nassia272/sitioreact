a28ab3d1184701ab718e4e3f37daddad34b6a494
Nassia272 <mildredherrera@gmail.com>
2021-07-16 07:39:36 -0500

    Version 1. MSJ nuevo y dos componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+import C01componente from "./componentes/C01componente";
+import C02contador from "./componentes/C02contador";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <C01componente/>
+      <hr />
+      <C02contador/>
+
     </div>
   );
 }
---
+++ src/componentes/C01componente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+            
+const C01componente = () => {
+    return (
+        <div>
+                Componente 1 definido con funcion flecha (rafce)        
+         </div>
+    )
+ }
+            
+export default C01componente
